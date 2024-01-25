1151877a11582815ea0a07389fe307ceb562f831
Erik Edin <ekedin@gmail.com>
2024-01-25 15:07:46 -0700

    added routes, models, and seeds according to instruction

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Ecommerce-backend
+
+  ## Description 
+
+  The purpose of this project was to create specific routes and models for a server backend.
+
+  This project was created to give the user a better understanding of the way in which developers create and execute their routes and models.
+
+  ## Table of Contents
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [Contributors](#contributors)
+
+  [Tests](#tests)
+
+  [License](#license)
+
+  [Questions](#questions)
+
+  ## Installation
+
+  the user can clone into the project through git bash and run `npm i` to install the dependencies.
+  the user will create and seed the database, then decide which http requests they want to use
+  the user then can use Insomnia or their IDE to view the results of the data in the database.
+  link to video walkthrough: https://drive.google.com/file/d/1mNZ_C0qdavXeexsq_NFOZio4LP_8HKI4/view
+
+  ## Usage 
+   - the user will clone the github project into their own machine, through their code studio, open a new terminal and install the dependencies with `npm i`  
+   - once the dependencies are downloaded, the user runs `mysql -u root -p` to initialize the mysql server
+   - once logged in, the user can then check if the database is linked properly with `source db/schema.sql`
+   - once seeded, run the program with either `npm run start` or `node server.js`
+   - the user can test routes by using the http requests in Insomnia to view the data that has been created.
+
+  ## Contributors 
+
+  Made with starter code from the University of Denver Full Stack Web Development Bootcamp.
+
+  ## Tests 
+  
+  the terminal used to run the application should notify the user of any errors in processing a request.
+
+  ## Questions 
+
+  Any Questions? Contact me on GitHub or email
+  https://github.com/ekedin123 
+
+  ekedin@gmail.com
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,49 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+  })
+    .then((productInfo) => res.json(productInfo))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+  })
+    .then((productInfo) => res.json(productInfo))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
@@ -27,17 +64,15 @@ router.post('/', (req, res) => {
   */
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        const productTagIdArray = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productTagIdArr);
+        return ProductTag.bulkCreate(productTagIdArray);
       }
-      // if no product tags, just respond
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
@@ -47,9 +82,8 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+// how to update the product information
 router.put('/:id', (req, res) => {
-  // update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -57,11 +91,9 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length) {
-
         ProductTag.findAll({
           where: { product_id: req.params.id }
         }).then((productTags) => {
-          // create filtered list of new tag_ids
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
           const newProductTags = req.body.tagIds
             .filter((tag_id) => !productTagIds.includes(tag_id))
@@ -71,12 +103,9 @@ router.put('/:id', (req, res) => {
                 tag_id,
               };
             });
-
-          // figure out which ones to remove
           const productTagsToRemove = productTags
             .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
             .map(({ id }) => id);
-          // run both actions
           return Promise.all([
             ProductTag.destroy({ where: { id: productTagsToRemove } }),
             ProductTag.bulkCreate(newProductTags),
@@ -87,13 +116,27 @@ router.put('/:id', (req, res) => {
       return res.json(product);
     })
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
-
+//how to delete by id val
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((productInfo) => {
+      if (!productInfo) {
+        res.status(404).json({ message: "Invalid Product ID." });
+        return;
+      }
+      res.json(productInfo);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
